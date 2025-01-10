eeb1ab5391b71763c35bc99cb101956341a5940e
billydev139 <hassanabdullah@softmindsol.com>
2025-01-10 17:00:37 +0500

    persist added to store theme configrations

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import { Provider } from "react-redux"; // Import Provider from react-redux
  // Import your Redux store
 import "./index.css";
 import App from "./App.jsx";
-import store from "./store/store/store.js";
+import store, { persistor } from "./store/store/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <App />
+    </PersistGate>
     </Provider>
   </StrictMode>
 );
