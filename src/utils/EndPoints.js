fe7230ff0ca21bd7889757458142189e3168344b
SabaSaeed71 <sabasaeed@softmindsol.com>
2025-02-26 11:29:40 +0500

    updated endpoints

--- src/utils/EndPoints.js
+++ src/utils/EndPoints.js
@@ -1,11 +1,8 @@
 
 const config = {
-    baseUrl: 'https://api.easydine.ch',
-    // baseUrl: 'http://localhost:7070',
-    imageUrl: `https://api.easydine.ch/uploads`,
-    // eslint-disable-next-line no-undef
-    // baseurl: import.meta.env.VITE_BASE_URL,
-    // imageUrl: import.meta.env.VITE_IMAGE_URL,
+  
+    baseUrl: import.meta.env.VITE_BASE_URL,
+    imageUrl: import.meta.env.VITE_IMAGE_URL,
     endPoints: {
       SUB_ADMIN: {
         LOGIN: "/auth/login",
