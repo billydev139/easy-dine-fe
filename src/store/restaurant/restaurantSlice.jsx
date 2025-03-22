2d214c5037626735147b76126ad6123c326d9dc9
Saba Saeed <sabasaeed@softmindsol.com>
2025-03-22 05:56:58 +0500

    added thunk function for edit delete getAll and getById

--- src/store/restaurant/restaurantSlice.jsx
+++ src/store/restaurant/restaurantSlice.jsx
@@ -9,6 +9,26 @@ const initialState = {
   errorMessage: "",
 };
 
+export const getAllRestaurant = createAsyncThunk(
+  "restaurant/getAllRestaurant",
+  async ({ page, limit, searchQuery }, { rejectWithValue }) => {
+    try {
+      const response = await axiosWithToken.get("/restaurants/", {
+        params: {
+          page, // Pagination page
+          limit, // Number of items per page
+          search: searchQuery, // Optional search query
+        },
+      });
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(
+        error.response ? error.response.data : error.message
+      );
+    }
+  }
+);
+
 // Define the asynchronous thunk for adding a new restaurant
 export const addNewRestaurant = createAsyncThunk(
   "restaurant/addNewRestaurant",
@@ -22,6 +42,46 @@ export const addNewRestaurant = createAsyncThunk(
   }
 );
 
+// Define the asynchronous thunk for deleting a restaurant
+export const deleteRestaurant = createAsyncThunk(
+  "restaurant/deleteRestaurant",
+  async (restaurantId, { rejectWithValue }) => {
+    try {
+      const response = await axiosWithToken.delete(`/restaurant/${restaurantId}`);
+      return response?.data; // Return the ID of the deleted restaurant to remove it from the state
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+
+// Define the asynchronous thunk for updating a restaurant
+export const updateRestaurant = createAsyncThunk(
+  "restaurant/updateRestaurant",
+  async ({ id, body }, { rejectWithValue }) => {
+    console.log("🚀 ~ body:", body);
+    try {
+      const response = await axiosWithToken.put(`/restaurant/${id}`, body);
+      return response.data; // Assuming response.data contains the updated vehicle info
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+// Define the asynchronous thunk for updating a restaurant
+export const getSingleRestaurant = createAsyncThunk(
+  "restaurant/getSingleRestaurant",
+  async (id, { rejectWithValue }) => {
+    try {
+      const response = await axiosWithToken.get(`/restaurant/${id}`);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+
+
 
 const restaurantSlice = createSlice({
     name: "restaurant",
@@ -29,6 +89,21 @@ const restaurantSlice = createSlice({
     reducers:{},
     extraReducers:(builder) => {
         builder
+        .addCase(getAllRestaurant.pending, (state) => {
+          state.isLoading = true;
+          state.isSuccess = false;
+          state.errorMessage = "";
+        })
+        .addCase(getAllRestaurant.fulfilled, (state, action) => {
+          state.isLoading = false;
+          state.isSuccess = true;
+          state.restaurant = action.payload;
+        })
+        .addCase(getAllRestaurant.rejected, (state, action) => {
+          state.isLoading = false;
+          state.isSuccess = false;
+          state.errorMessage = action.payload.message;
+        })
         .addCase(addNewRestaurant.pending, (state) => {
           state.isLoading = true;
           state.isSuccess = false;
@@ -40,6 +115,55 @@ const restaurantSlice = createSlice({
           state.restaurant = action.payload;
         })
         .addCase(addNewRestaurant.rejected, (state, action) => {
+          state.isLoading = false;
+          state.isSuccess = false;
+          state.errorMessage = action.payload.message;
+        })
+        .addCase(deleteRestaurant.pending, (state) => {
+          state.isLoading = true;
+          state.isSuccess = false;
+          state.errorMessage = "";
+        })
+        .addCase(deleteRestaurant.fulfilled, (state) => {
+          state.isLoading = false;
+          state.isSuccess = true;
+          state.errorMessage = "";
+          // state.restaurant = state.restaurant.filter(
+          //   (restaurant) => restaurant.id !== action.payload
+          // );
+        })
+        .addCase(deleteRestaurant.rejected, (state, action) => {
+          state.isLoading = false;
+          state.isSuccess = false;
+          state.errorMessage = action.payload.message;
+        })
+        .addCase(updateRestaurant.pending, (state) => {
+          state.isLoading = true;
+          state.isSuccess = false;
+          state.errorMessage = "";
+        })
+        .addCase(updateRestaurant.fulfilled, (state, action) => {
+          state.isLoading = false;
+          state.isSuccess = true;
+          state.errorMessage = "";
+          state.singleRestaurant = action.payload;
+        })
+        .addCase(updateRestaurant.rejected, (state, action) => {
+          state.isLoading = false;
+          state.isSuccess = false;
+          state.errorMessage = action.payload.message;
+        })
+        .addCase(getSingleRestaurant.pending, (state) => {
+          state.isLoading = true;
+          state.isSuccess = false;
+          state.errorMessage = "";
+        })
+        .addCase(getSingleRestaurant.fulfilled, (state, action) => {
+          state.isLoading = false;
+          state.isSuccess = true;
+          state.singleRestaurant = action.payload;
+        })
+        .addCase(getSingleRestaurant.rejected, (state, action) => {
           state.isLoading = false;
           state.isSuccess = false;
           state.errorMessage = action.payload.message;
